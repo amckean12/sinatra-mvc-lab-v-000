25beb1a0e36e70abf073c14f8a2f2ca703911f43
Alex McKean <amckean@me.com>
2018-04-12 16:23:10 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,11 @@
 class PigLatinizer
   attr_reader :text
-  attr_accessor 
+  attr_accessor :result
 
   @@vowels = ["a", "e", "i", "o", "u"]
   @@twoLetterConst = ["ch", "sh", "qu", "th", "br"]
   @@threeLetterConst = ["thr", "sch", "squ"]
-  
+
 
   def initialize(text)
     @text = text.split(" ")
@@ -15,23 +15,20 @@ class PigLatinizer
     @result = []
     @text.each do |word|
       if @@vowels.include? word[0]
-        result.push word << 'ay'
-      else 
+        @result.push word << 'ay'
+      else
         if @@twoLetterConst.include? word[0] + word[1] + word[2]
           firstThreeLetters = word.slice!(0,3)
-          result.push word << firstThreeLetters << 'ay'
+          @result.push word << firstThreeLetters << 'ay'
         elsif twoLetterConst.include? word[0] + word[1]
           firstTwoLetters = word.slice!(0,2)
-          resul.push word << firstTwoLetters << 'ay'
-        else 
+          @result.push word << firstTwoLetters << 'ay'
+        else
           firstLetter = word.slice!(0)
-          result.push word << firstLetter << 'ay'
-        end 
-      end 
-    end 
-      return result.join(" ")    
-  end
-
-  def createPigLatinPhrase
+          @result.push word << firstLetter << 'ay'
+        end
+      end
+    end
+      @pigResult = @result.join(" ")
   end
 end
